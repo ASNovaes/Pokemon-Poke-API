395f90f68187ddb93c6c60886ceec9a7c5737bd4
Alex Novaes <alex-novaes@hotmail.com>
2020-07-23 23:04:33 -0300

    add

--- js/main.js
+++ js/main.js
@@ -39,8 +39,7 @@ const templateCardPoke = (pokemon, container = content, hasEvent = true) => {
 
     const { id, name } = pokemon;
     let color = paintCardAccordingToType(pokemon);
-    let urlImage = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`
-
+    let urlImage = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
 
     container.innerHTML += `
        <div class="card" data-color=${color} data-id=${id}>
